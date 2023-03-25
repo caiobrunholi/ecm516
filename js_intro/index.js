4dc76c78d905f65b0eb97a6b87455c6131fb8a71
caiobrunholi <caio.brunholi@hotmail.com>
2023-03-25 09:16:47 -0300

    fim aula5

--- js_intro/index.js
+++ js_intro/index.js
@@ -508,53 +508,95 @@
 // Aula 5
 // API KEY OPENWEATHERMAP: 187e76d9b6c8bdafe9bf023428fed59e
 
-const axios = require('axios')
+// const axios = require('axios')
 
-const appid = '187e76d9b6c8bdafe9bf023428fed59e';
-const q = 'Itu'
-const units = 'metric'
-const lang = 'pt_BR'
-// quantidade de resultados
-const cnt = '10'
+// const appid = '187e76d9b6c8bdafe9bf023428fed59e';
+// const q = 'Itu'
+// const units = 'metric'
+// const lang = 'pt_BR'
+// // quantidade de resultados
+// const cnt = '10'
 
-const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=${units}&appid=${appid}&lang=${lang}&cnt=${cnt}`;
+// const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=${units}&appid=${appid}&lang=${lang}&cnt=${cnt}`;
 
-//faz a requisição
-axios
-    .get(url)
-    .then((res) => {
-        //mostra o resultado e devolve somente a parte de interesse
-        console.log(res);
-        return res.data;
-    })
-    .then((res) => {
-        //mostra o total e devolve o resultado
-        console.log(res.cnt);
-        return res;
-    })
-    .then((res) => {
-        //devolve somente a lista de previsões
-        console.log("aqui", res);
-        return res["list"];
-    })
-    .then((res) => {
-        //para cada resultado, mostra algumas informações
-        for (let previsao of res) {
-            console.log(`
-                ${new Date(+previsao.dt * 1000).toLocaleString()},
-                ${'Min: ' + previsao.main.temp_min}\u00B0C,
-                ${'Max: ' + previsao.main.temp_max}\u00B0C,
-                ${'Hum: ' + previsao.main.humidity}%,
-                ${previsao.weather[0].description}
-            `);
-        }
-        return res;
-    })
-    .then((res) => {
-        //verifica quantas previsões têm percepção humana tempertura acima de 30 graus
-        const lista = res.filter(r => r.main.feels_like >= 30);
-        console.log(`${lista.length} previsões têm percepção humana de temperatura acima de 30 graus`)  
-     })
-    .catch ((err) => {
-    console.log(err)
-})
+// //faz a requisição
+// axios
+//     .get(url)
+//     .then((res) => {
+//         //mostra o resultado e devolve somente a parte de interesse
+//         console.log(res);
+//         return res.data;
+//     })
+//     .then((res) => {
+//         //mostra o total e devolve o resultado
+//         console.log(res.cnt);
+//         return res;
+//     })
+//     .then((res) => {
+//         //devolve somente a lista de previsões
+//         console.log("aqui", res);
+//         return res["list"];
+//     })
+//     .then((res) => {
+//         //para cada resultado, mostra algumas informações
+//         for (let previsao of res) {
+//             console.log(`
+//                 ${new Date(+previsao.dt * 1000).toLocaleString()},
+//                 ${'Min: ' + previsao.main.temp_min}\u00B0C,
+//                 ${'Max: ' + previsao.main.temp_max}\u00B0C,
+//                 ${'Hum: ' + previsao.main.humidity}%,
+//                 ${previsao.weather[0].description}
+//             `);
+//         }
+//         return res;
+//     })
+//     .then((res) => {
+//         //verifica quantas previsões têm percepção humana tempertura acima de 30 graus
+//         const lista = res.filter(r => r.main.feels_like >= 30);
+//         console.log(`${lista.length} previsões têm percepção humana de temperatura acima de 30 graus`)  
+//      })
+//     .catch ((err) => {
+//     console.log(err)
+// })
+
+
+
+
+// async function hello (nome){
+//     return 'Oi, '+nome
+// }
+
+// const boasVindas = hello('João')
+// console.log(boasVindas)
+// boasVindas.then((res) => console.log(res))
+
+
+function fatorial(n) {
+    if (n < 0) return Promise.reject("Valor não pode ser negativo");
+    let res = 1;
+    for (let i = 2; i <= n; i++) res *= i;
+    return Promise.resolve(res);
+}
+
+function chamadaComThenCatch() {
+    fatorial(5)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    fatorial(-1)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+}
+chamadaComThenCatch();
+
+async function chamadaComAwait(){
+    const f1 = await fatorial(5)
+    console.log(f1)
+    try{
+        const f2 = await fatorial(-1)
+        console.log(f2)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+chamadaComAwait()
